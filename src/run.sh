fc428393791feca50850e6e36b6468d2b9d0ac6e
Kay Oh <khtdr.com@gmail.com>
2020-04-05 15:55:36 -0600

    simpler launcher

--- src/run.sh
+++ src/run.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
+
 cd "$(dirname "$0")/.." || exit
 
-if [ ! -t 0 ]; then
-    lines=""
-    while IFS= read -r line; do
-        lines="${lines}"$'\n'"${line}"
-    done
-fi
-
-echo "$lines" | node bin/stoa.d "$@"
+node bin/stoa.d "$@"
