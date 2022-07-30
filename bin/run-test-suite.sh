8b2ad144a526ec2e16dcbec9956f20207988d393
Kay Oh <khtdr.com@gmail.com>
2022-07-30 17:21:27 -0600

    test output

--- bin/run-test-suite.sh
+++ bin/run-test-suite.sh
@@ -6,53 +6,55 @@ export PATH=../bin:./:$PATH
 test_stage() {
     stderr_TEMP=$(mktemp)
     stdout_TEMP=$(mktemp)
-    ./$test_script $flags 1>/dev/null 2> $stderr_TEMP
-    ./$test_script $flags 2>/dev/null 1> $stdout_TEMP
-    stderr_RESP=$(diff $stderr_TEMP "${test_script}.${ext}.stderr")
-    stdout_RESP=$(diff $stdout_TEMP "${test_script}.${ext}.stdout")
+    "./$test_script" "$flags" 1>/dev/null 2> "$stderr_TEMP"
+    "./$test_script" "$flags" 2>/dev/null 1> "$stdout_TEMP"
+    stderr_RESP=$(diff "$stderr_TEMP" "${test_script}.${ext}.stderr")
+    stdout_RESP=$(diff "$stdout_TEMP" "${test_script}.${ext}.stdout")
+
     if [ -z "${stderr_RESP}" ]; then
-      echo "  ✔ ${err_title}"
+      echo -n "✔"
       ((pass+=1))
     else
-      echo "  ❌ ${err_title}"
+      echo -n "✖"
+      echo "${ext}.stdout"
       echo 'ACTUAL < -- > EXPECTED'
       echo "${stderr_RESP}"
       ((fail+=1))
     fi
     if [ -z "${stdout_RESP}" ]; then
-      echo "  ✔ ${out_title}"
+      echo -n "✔"
       ((pass+=1))
     else
-      echo "  ❌ ${out_title}"
+      echo -n "✖"
+      echo "${ext}.stdout"
       echo 'ACTUAL < -- > EXPECTED'
       echo "${stdout_RESP}"
       ((fail+=1))
     fi
-    rm $stderr_TEMP
-    rm $stdout_TEMP
+    rm "$stderr_TEMP"
+    rm "$stdout_TEMP"
 }
 
 pass=0
 fail=0
 for test_script in *.stoa; do
-    echo Testing: $test_script
+    echo Testing: "${test_script}"
     flags="-t"
     ext="tokens"
-    err_title="Token errors"
-    out_title="Token output"
     test_stage
 
     flags="-p"
     ext="ast"
-    err_title="Parse errors"
-    out_title="Parse tree"
     test_stage
 
     flags=""
     ext="eval"
-    err_title="Runtime errors"
-    out_title="Runtime output"
     test_stage
+
+    echo
 done
-echo Passing: $pass Failing: $fail
+echo
+total=$((pass+fail))
+echo -e "Passing: $pass out of $total ($((pass*100/total))%)"
+echo "Failing: $fail out of $total ($((fail*100/total))%)"
 echo
